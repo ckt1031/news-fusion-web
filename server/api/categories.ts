bea23e4eb60d80a1ac9b762a908f886dbf08255e
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-01-10 11:48:32 +0800

    feat(categories.ts): add new categories to allowed categories array to expand the scope of the app

--- server/api/categories.ts
+++ server/api/categories.ts
@@ -1,4 +1,4 @@
-export const allowedCategories = ["world", "technology", "ai"]
+export const allowedCategories = ["world", "technology", "ai", "finance", "investing", "dev", "free-games", "hong-kong"]
 
 export default defineEventHandler((event) => {
     return allowedCategories
