2779c87e872158d56e5fbc4a65dee7a0724cf6ba
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-01-11 19:41:15 +0800

    feat(nuxt.config.ts): add nuxt-security module for enhanced security feat(nuxt.config.ts): add vite-plugin-remove-console to remove console logs in production feat(nuxt.config.ts): configure Content Security Policy to improve security feat(package.json): add nuxt-security and vite-plugin-remove-console dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
+import removeConsole from "vite-plugin-remove-console";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: "2024-11-01",
-	modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxtjs/mdc"],
+	modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxtjs/mdc", "nuxt-security"],
 	devtools: { enabled: true },
 	fonts: {
 		provider: "google",
@@ -11,5 +13,39 @@ export default defineNuxtConfig({
 	},
 	experimental: {
 		inlineRouteRules: true,
+	},
+
+	// Vite plugin options
+	vite: {
+		// Options for Vite plugin
+		plugins: [removeConsole()],
+	},
+
+	security: {
+		sri: true,
+		hidePoweredBy: false,
+		headers: {
+			contentSecurityPolicy: {
+				// Images
+				"img-src": [
+					"'self'",
+					"https://www.google.com",
+					"data:",
+					"https://*.gstatic.com",
+				],
+				// Styles
+				"style-src": [
+					"'self'",
+					"'unsafe-inline'",
+					"https://fonts.googleapis.com",
+				],
+				// Scripts
+				"script-src": [
+					"'self'",
+					"'unsafe-inline'",
+					"https://static.cloudflareinsights.com",
+				],
+			},
+		},
 	},
 });
