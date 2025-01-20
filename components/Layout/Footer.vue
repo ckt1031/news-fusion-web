cf68bfaa012e2f7b2af17d8437ed00cf8dd35dbc
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-01-20 14:38:59 +0800

    feat: theme switcher

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -3,22 +3,29 @@
 </script>
 
 <template>
-  <footer class="flex justify-center items-center h-16 mt-10 mb-8 flex-col md:flex-row text-gray-600 dark:text-gray-300 gap-2">
-    <p class="items-center">
-      {{ new Date().getFullYear() }}
-      <a href="/about" class="hover:underline">
-        News Fusion
-      </a>
-    </p>
-    <p>
-      &#x2022;
-    </p>
-    <p class="items-center">
-      Made with ❤️ by
-      <a href="https://github.com/ckt1031" target="_blank" class="text-blue-500 dark:text-blue-400 hover:underline">
-        ckt1031
-      </a>
-    </p>
+  <footer class="flex flex-col justify-center items-center gap-3 mt-8 mb-10 md:mb-5 h-16">
+    <div
+        class="flex justify-center items-center flex-col md:flex-row text-gray-600 dark:text-gray-300 gap-0.5 sm:gap-2"
+    >
+      <p class="items-center">
+        {{ new Date().getFullYear() }}
+        <a href="/about" class="hover:underline">
+          News Fusion
+        </a>
+      </p>
+      <p>
+        &#x2022;
+      </p>
+      <p class="items-center">
+        Made with ❤️ by
+        <a href="https://github.com/ckt1031" target="_blank" class="text-blue-500 dark:text-blue-400 hover:underline">
+          ckt1031
+        </a>
+      </p>
+    </div>
+    <ClientOnly>
+      <ThemeSwitcher/>
+    </ClientOnly>
   </footer>
 </template>
 
---
+++ components/Layout/ThemeSwitcher.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import clsx from "clsx";
+
+const colorMode = useColorMode();
+const preference = ref<string>(colorMode.preference);
+const setPref = (pref: string) => {
+	preference.value = pref;
+	colorMode.preference = pref;
+};
+const buttonClass = "p-1.5";
+const selectedClass =
+	"text-black dark:text-white bg-neutral-100 dark:bg-neutral-800 rounded-full border border-neutral-300 dark:border-neutral-700";
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <div class="items-center rounded-full border border-neutral-300 dark:border-neutral-700">
+      <div class="flex flex-row gap-0.5 p-1">
+        <UTooltip text="Change to System Theme">
+          <UButton
+              icon="i-hugeicons-computer"
+              size="xs"
+              :padded="false"
+              color="gray"
+              variant="link"
+              title="Auto Mode"
+              :class="clsx(buttonClass, preference === 'system' ? selectedClass : '')"
+              @click="setPref('system')"
+          />
+        </UTooltip>
+        <UTooltip text="Change to Light Theme">
+          <UButton
+              icon="i-hugeicons-bulb"
+              size="xs"
+              :padded="false"
+              color="gray"
+              variant="link"
+              title="Light Mode"
+              :class="clsx(buttonClass, preference === 'light' ? selectedClass : '')"
+              @click="setPref('light')"
+          />
+        </UTooltip>
+        <UTooltip text="Change to Dark Theme">
+          <UButton
+              icon="i-hugeicons-moon-02"
+              size="xs"
+              :padded="false"
+              color="gray"
+              variant="link"
+              title="Dark Mode"
+              :class="clsx(buttonClass, preference === 'dark' ? selectedClass : '')"
+              @click="setPref('dark')"
+          />
+        </UTooltip>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
