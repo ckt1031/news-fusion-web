f9091f3e9d3d4428fe961733d12c4dbefe845d75
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-02-01 14:52:58 +0800

    refactor: enhance styling of FeedPublisher component for improved layout and appearance

--- components/Feed/Item/FeedPublisher.vue
+++ components/Feed/Item/FeedPublisher.vue
@@ -7,7 +7,7 @@ const url = `https://www.google.com/s2/favicons?domain=${props.url}&sz=64`;
 </script>
 
 <template>
-  <div class="flex flex-row items-center gap-1">
+  <div class="flex flex-row items-center gap-1 px-2 py-1 border-1 border-zinc-300 dark:border-zinc-700 rounded-lg">
     <img class="w-4 h-4" alt="favicon" :src="url"/>
     <span class="text-zinc-500 dark:text-zinc-400">{{ props.name }}</span>
   </div>
