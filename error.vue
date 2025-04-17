f36a0ec5546bdf6189ec87209b23f3c67f6cdcab
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-04-17 14:41:38 +0800

    refactor(error.vue): restructure template for improved readability

--- error.vue
+++ error.vue
@@ -22,26 +22,28 @@ useHead({ title });
 </script>
 
 <template>
-  <Global>
-    <div class="h-full flex flex-col items-center justify-center text-white">
-      <div v-if="error" class="flex flex-col md:flex-row items-center gap-5 md:gap-10">
-        <div class="md:border-r-2 border-zinc-700 h-full flex flex-col items-center justify-center">
-          <h2 class="text-7xl font-bold md:mr-10 justify-self-center">{{ error.statusCode }}</h2>
-        </div>
-        <div class="flex flex-col gap-2 md:text-lg">
-          <div v-if="error.statusCode === 404" class="flex flex-col gap-2 items-center md:items-start">
-            <h1 class="text-2xl font-bold">Page Not Found</h1>
-            <p class="text-lg">The page you are looking for does not exist.</p>
+	<UApp>
+    <Global>
+      <div class="h-full flex flex-col items-center justify-center text-white">
+        <div v-if="error" class="flex flex-col md:flex-row items-center gap-5 md:gap-10">
+          <div class="md:border-r-2 border-zinc-700 h-full flex flex-col items-center justify-center">
+            <h2 class="text-7xl font-bold md:mr-10 justify-self-center">{{ error.statusCode }}</h2>
           </div>
-          <code v-else class="bg-black text-sm p-2 rounded-md font-mono max-w-64 max-h-36 overflow-auto">
-            {{ error.message }}
-          </code>
-          <button @click="handleError"
-                  class="mt-4 bg-zinc-900 border border-zinc-700 px-4 py-2 rounded-md hover:bg-zinc-800 transition-colors">
-            Back to Home
-          </button>
+          <div class="flex flex-col gap-2 md:text-lg">
+            <div v-if="error.statusCode === 404" class="flex flex-col gap-2 items-center md:items-start">
+              <h1 class="text-2xl font-bold">Page Not Found</h1>
+              <p class="text-lg">The page you are looking for does not exist.</p>
+            </div>
+            <code v-else class="bg-black text-sm p-2 rounded-md font-mono max-w-64 max-h-36 overflow-auto">
+              {{ error.message }}
+            </code>
+            <button @click="handleError"
+                    class="mt-4 bg-zinc-900 border border-zinc-700 px-4 py-2 rounded-md hover:bg-zinc-800 transition-colors">
+              Back to Home
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  </Global>
+    </Global>
+  </UApp>
 </template>
